a27ff6d28dc0b9a451113f9552d4894dbc38d3b7
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-11-24 15:30:39 +0700

    Add project files.

---
+++ exercise3_20210140085/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise3_20210140085
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
