b80b4a6725e95fc4e97d632c8a9076ebccea2cc2
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-11-24 16:17:06 +0700

    change case 4 to 5 and add case 4 for addNode

--- exercise3_20210140085/Program.cs
+++ exercise3_20210140085/Program.cs
@@ -121,8 +121,9 @@ namespace exercise3_20210140085
                     Console.WriteLine("1. View all the records in the list");
                     Console.WriteLine("2. Search for a record in the list");
                     Console.WriteLine("3. Display the first record in the list");
-                    Console.WriteLine("4. Exit");
-                    Console.WriteLine("\nEnter your choice (1-4): ");
+                    Console.WriteLine("4. Add data in the list");
+                    Console.WriteLine("5. Exit");
+                    Console.WriteLine("\nEnter your choice (1-5): ");
                     char ch = Convert.ToChar(Console.ReadLine());
                     switch (ch)
                     {
@@ -158,6 +159,11 @@ namespace exercise3_20210140085
                             }
                             break;
                         case '4':
+                            {
+                                obj.addNode();
+                            }
+                            break;
+                        case '5':
                             return;
                         default:
                             {
