3ad71362c3093f034c64414ad26c908720ef286c
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-11-24 15:37:34 +0700

    add method Search in class CircularList for search data

--- exercise3_20210140085/Program.cs
+++ exercise3_20210140085/Program.cs
@@ -20,6 +20,19 @@ namespace exercise3_20210140085
         {
             LAST = null;
         }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = LAST.next; current != LAST;
+                previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);
+            }
+            if (rollNo == LAST.rollNumber)
+                return true;
+            else
+                return (false);
+        }
     }
     internal class Program
     {
